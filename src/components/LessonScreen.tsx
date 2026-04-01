13c319783d2b26596deed9d4e2ae549091203a62
5hillkids-cpu <5hillkids@gmail.com>
2026-04-01 09:47:00 -0400

    Restore full LessonScreen component and add lesson1 background classes based on current slide.

--- src/components/LessonScreen.tsx
+++ src/components/LessonScreen.tsx
@@ -1,3 +1,15 @@
-<div className="slide-content lesson-1-bg">
-  {/* Lesson 1 content here */}
-</div>
+// Full LessonScreen Component
+import React from 'react';
+import './LessonScreen.css';
+
+const LessonScreen = ({ currentSlide }) => {
+    const backgroundClass = `background-${currentSlide}`;
+    return (
+        <div className={`lesson-screen ${backgroundClass}`}> 
+            <h1>Lesson Screen</h1>
+            {/* Other component code here */}
+        </div>
+    );
+};
+
+export default LessonScreen;
